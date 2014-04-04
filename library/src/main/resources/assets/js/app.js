97fd2150826ea70d5d76d23e7f1b288cd4a42bd9
kemiporto <claudiafurushima@gmail.com>
2014-04-04 10:51:47 -0700

    2014-04-04_1051 checkout

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,16 @@
 $(":button").click(function() {
 	var isbn = this.id;
 	alert('About to report lost on ISBN ' + isbn);
+
+    $.ajax({
+	type: "PUT",
+	contentType: "application/json",
+	url: "http://localhost:8001/library/v1/books/" + isbn + "?status=lost",
+	success: function (msg) {
+            alert('Success: book ' + isbn + ' status changed to lost');
+	},
+	error: function (err){
+            alert('Error. Please try again.');
+	}
+    });
 });
